871bbc8d8415c22cced3f4dcd65afa9eaafcb410
Niferu <wargacki.patryq@gmail.com>
2023-11-18 14:45:57 +0100

    header fixed

--- components/header.tsx
+++ components/header.tsx
@@ -11,7 +11,7 @@ const montserrat = Space_Grotesk({ subsets: ["latin"] })
 export default function Header() {
     const [navBtn, setNavBtn] = useState(false)
 
-    /** @dev Always set value to opposite to current one */
+    /** @dev Always set value opposite to current one */
     const handleNavBtn = () => setNavBtn(!navBtn)
 
     /** @dev Update navBtn based on screen width */
@@ -32,11 +32,13 @@ export default function Header() {
 
     return (
         <header className="flex w-full h-[4.5rem] bg-transparent">
-            <Image className="pl-[2rem]" src="/icon.png" alt="DigitalRightsMaykr" height="100" width="100" quality="95" priority={true}></Image>
-            <h1 className={`${montserrat.className} flex ml-[2rem] items-center w-[25rem] text-3xl text-white font-bold`}>Digital Rights Maykr</h1>
+            <Image className="pl-[2rem] xl:ml-[3rem]" src="/icon.png" alt="DigitalRightsMaykr" height="100" width="100" quality="95" priority={true}></Image>
+            <h1 className={`${montserrat.className} fixed mt-[1.25rem] ml-[6rem] xl:ml-[9rem] text-center w-[20rem] text-3xl text-white font-bold`}>
+                Digital Rights Maykr
+            </h1>
 
             {/* Normal Version Of Navigation Bar */}
-            <nav className="md:flex hidden justify-end gap-4 right-0 w-full items-center text-white list-none">
+            <nav className="lg:flex hidden justify-end gap-4 mr-[1.5rem] w-full items-center text-white list-none">
                 {links.map((link) => (
                     <li key={link.hash}>
                         <Link
@@ -50,15 +52,13 @@ export default function Header() {
                         </Link>
                     </li>
                 ))}
-
-                <ConnectButton moralisAuth={false} />
             </nav>
 
             {/* Mobile Version Of Navigation Bar */}
             <nav
                 className={
                     navBtn
-                        ? "md:hidden text-center items-center text-white list-none absolute w-full top-[4.6rem] flex-col bg-black bg-opacity-70 backdrop-blur-[5px] rounded-b-lg"
+                        ? "lg:hidden text-center items-center text-white list-none absolute w-full top-[4.6rem] flex-col bg-black bg-opacity-70 backdrop-blur-[5px] rounded-b-lg"
                         : "hidden"
                 }
             >
@@ -81,7 +81,10 @@ export default function Header() {
                     <ConnectButton moralisAuth={false} />
                 </div>
             </nav>
-            <div className={"flex md:hidden self-center ml-auto mr-[3rem] text-2xl text-white hover:cursor-pointer"}>
+            <div className="hidden lg:flex absolute right-0 top-[4.5rem]">
+                <ConnectButton moralisAuth={false} />
+            </div>
+            <div className={"flex lg:hidden self-center ml-auto mr-[3rem] text-2xl text-white hover:cursor-pointer"}>
                 {navBtn ? <FaTimes onClick={handleNavBtn} /> : <FaBars onClick={handleNavBtn} />}
             </div>
         </header>
