ff0a515d7445d1ab84c267c1e4a047c0ede2abe2
Niferu <wargacki.patryq@gmail.com>
2024-02-02 00:07:30 +0100

    footer animations added

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,13 @@
+import { motion } from "framer-motion"
+
 export default function Footer() {
     return (
-        <footer className="mt-auto mb-10 px-4 text-center text-gray-500">
+        <motion.footer
+            className="mt-auto mb-10 px-4 text-center text-gray-500"
+            initial={{ y: 100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay: 0.75, duration: 0.5 }}
+        >
             <small className="mb-2 block text-xs">&copy; 2023 Niferu. All rights reserved.</small>
             <p className="text-xs">
                 <span className="font-semibold">About this website:</span> built with React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
@@ -9,6 +16,6 @@ export default function Footer() {
             <p className="text-xs">
                 <span className="font-semibold">Backend built with:</span> Solidity, Hardhat & Typescript. Data straight from blockchain.
             </p>
-        </footer>
+        </motion.footer>
     )
 }
